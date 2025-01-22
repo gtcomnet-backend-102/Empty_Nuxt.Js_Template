3d62eae77083623cfe0a4614a2369a5fe1ffeb51
gtcomnet-backend-102 <kelechi.ndubuisi@gtcomnet.com>
2025-01-22 12:19:51 +0100

    first push

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<HeaderPage />
+  <Tutorial/>
+  <FooterPage />
+</div>
+</template>
+
+<script>
+import HeaderPage from '~/components/HeaderPage.vue'
+import FooterPage from '~/components/FooterPage.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    HeaderPage,
+    FooterPage
+  }
+}
+</script>
